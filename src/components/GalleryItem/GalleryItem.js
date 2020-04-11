4d2d65258ed1ff96801dce54dbddeea8c6b21060
Pamela Anderson <cyberryder2010@gmail.com>
2020-04-11 16:48:11 -0500

    Like Button position under img

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -3,14 +3,18 @@ import React, { Component } from "react";
 class GalleryItem extends Component {
   render() {
     return (
-      <div className="gallery">
-        <img src={this.props.item.path} alt={this.props.item.description} />
-        <button
-          className="likes"
-          onClick={this.props.addLikes(this.props.item.id)}
-        >
-          LIKE ME
-        </button>
+      <div className="galleryItems">
+        <div className="images">
+          <img src={this.props.item.path} alt={this.props.item.description} />
+        </div>
+        <div className="button">
+          <button
+            className="likes"
+            onClick={this.props.addLikes(this.props.item.id)}
+          >
+            LIKE ME
+          </button>
+        </div>
       </div>
     );
   }
